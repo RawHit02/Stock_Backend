baa4d30bcbcdf67b295bc6ca5bf67f91f4cfba42
dabralakshat274 <dabralakshat274@gmail.com>
2024-11-13 01:35:57 +0530

    added columns -contactNumber,whatsappNumber,email,address to VendorManagementTbl table

--- src/infrastructure/data-access/entities/vendor-management/vendor_management.entity.ts
+++ src/infrastructure/data-access/entities/vendor-management/vendor_management.entity.ts
@@ -23,6 +23,42 @@ export class VendorManagementEntity extends EntityBase {
 
   @AutoMap()
   @Column({
+    name: 'ContactNumber',
+    type: 'character varying',
+    length: 15,
+    default: 'N/A', // Default value instead of nullable
+  })
+  contactNumber: string;
+
+  @AutoMap()
+  @Column({
+    name: 'WhatsappNumber',
+    type: 'character varying',
+    length: 15,
+    default: 'N/A', 
+  })
+  whatsappNumber: string;
+
+  @AutoMap()
+  @Column({
+    name: 'Email',
+    type: 'character varying',
+    length: 150,
+    unique: true,
+    default: 'noemail@example.com', 
+  })
+  email: string;
+
+  @AutoMap()
+  @Column({
+    name: 'Address',
+    type: 'text',
+    default: 'Unknown', 
+  })
+  address: string;
+
+  @AutoMap()
+  @Column({
     name: 'IsDeleted',
     type: 'boolean',
     default: false,
